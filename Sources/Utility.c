26141fa901e10fa95ced70d86c0ffcd74696dc15
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2022-09-24 00:34:07 +0200

    Added little endian UTF-16 support to UtilityConvertString() and refactored the logs in the Utility module.

--- Sources/Utility.c
+++ Sources/Utility.c
@@ -4,6 +4,7 @@
  */
 #include <errno.h>
 #include <iconv.h>
+#include <Log.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
@@ -24,7 +25,7 @@ int UtilityCreateDirectory(char *Pointer_Directory_Name)
 		// The directory is existing, make sure this is really a directory
 		if (!(Status.st_mode & S_IFDIR))
 		{
-			printf("Error : the \"%s\" file already exists but is not a directory.\n", Pointer_Directory_Name);
+			LOG("Error : the \"%s\" file already exists but is not a directory.\n", Pointer_Directory_Name);
 			return -1;
 		}
 	}
@@ -33,14 +34,14 @@ int UtilityCreateDirectory(char *Pointer_Directory_Name)
 		// Do not consider the "directory does not exist" error as we want to create a directory that may not exist yet
 		if (errno != ENOENT)
 		{
-			printf("Error : could not get directory \"%s\" status (%s).\n", Pointer_Directory_Name, strerror(errno));
+			LOG("Error : could not get directory \"%s\" status (%s).\n", Pointer_Directory_Name, strerror(errno));
 			return -1;
 		}
 
 		// Try to create the directory with standard user permissions
 		if (mkdir(Pointer_Directory_Name, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH) != 0)
 		{
-			printf("Error : could not create the directory (%s)\n", strerror(errno));
+			LOG("Error : could not create the directory (%s)\n", strerror(errno));
 			return -1;
 		}
 	}
@@ -56,6 +57,8 @@ int UtilityConvertString(void *Pointer_String_Source, void *Pointer_String_Desti
 		"WINDOWS-1252",
 		// UTILITY_CHARACTER_SET_UTF16_BIG_ENDIAN
 		"UTF-16BE",
+		// UTILITY_CHARACTER_SET_UTF16_LITTLE_ENDIAN
+		"UTF-16LE",
 		// UTILITY_CHARACTER_SET_UTF8
 		"UTF-8"
 	};
@@ -67,13 +70,13 @@ int UtilityConvertString(void *Pointer_String_Source, void *Pointer_String_Desti
 	// Select the correct character sets
 	if ((Source_Character_Set < 0) || (Source_Character_Set >= UTILITY_CHARACTER_SETS_COUNT))
 	{
-		printf("Error : unknown source character set %d, aborting string conversion.\n", Source_Character_Set);
+		LOG("Error : unknown source character set %d, aborting string conversion.\n", Source_Character_Set);
 		return Return_Value;
 	}
 	Pointer_String_Source_Character_Set = Pointer_String_Character_Sets[Source_Character_Set];
 	if ((Destination_Character_Set < 0) || (Destination_Character_Set >= UTILITY_CHARACTER_SETS_COUNT))
 	{
-		printf("Error : unknown destination character set %d, aborting string conversion.\n", Destination_Character_Set);
+		LOG("Error : unknown destination character set %d, aborting string conversion.\n", Destination_Character_Set);
 		return Return_Value;
 	}
 	Pointer_String_Destination_Character_Set = Pointer_String_Character_Sets[Destination_Character_Set];
@@ -82,7 +85,7 @@ int UtilityConvertString(void *Pointer_String_Source, void *Pointer_String_Desti
 	Conversion_Descriptor = iconv_open(Pointer_String_Destination_Character_Set, Pointer_String_Source_Character_Set);
 	if (Conversion_Descriptor == (iconv_t) -1)
 	{
-		printf("Error : failed to create the character set conversion descriptor, aborting text conversion.\n");
+		LOG("Error : failed to create the character set conversion descriptor, aborting text conversion.\n");
 		return Return_Value;
 	}
 
@@ -93,7 +96,7 @@ int UtilityConvertString(void *Pointer_String_Source, void *Pointer_String_Desti
 	Pointer_String_Source_Characters = Pointer_String_Source;
 	Pointer_String_Destination_Characters = Pointer_String_Destination;
 	Destination_String_Available_Bytes = Destination_String_Size;
-	if (iconv(Conversion_Descriptor, &Pointer_String_Source_Characters, &Source_String_Size, &Pointer_String_Destination_Characters, &Destination_String_Available_Bytes) == (size_t) -1) printf("Error : text conversion failed (%s).\n", strerror(errno));
+	if (iconv(Conversion_Descriptor, &Pointer_String_Source_Characters, &Source_String_Size, &Pointer_String_Destination_Characters, &Destination_String_Available_Bytes) == (size_t) -1) LOG("Error : text conversion failed (%s).\n", strerror(errno));
 	else Return_Value = (int) (Destination_String_Size - Destination_String_Available_Bytes); // Determine how many bytes of the destination buffer have been used
 	iconv_close(Conversion_Descriptor);
 
